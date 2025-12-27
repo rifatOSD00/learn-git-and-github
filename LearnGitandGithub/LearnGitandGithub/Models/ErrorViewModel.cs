d2c46367e21e3a99188d980acd40c2d0406b32c2
MD Rubyait <MD Rubyait@DESKTOP-801P4FJ>
2025-12-27 20:04:04 +0600

    MVC project created

---
+++ LearnGitandGithub/LearnGitandGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnGitandGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
